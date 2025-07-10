b708ca966271938eeef4133162223752ce8cfd91
SyedAsharBukhari <asharbuhari649@gmail.com>
2025-07-10 03:03:31 -0700

    working

--- login.js
+++ login.js
@@ -2,14 +2,10 @@ const supaUrl = "https://xtjuukzhvksfolnjwvzr.supabase.co"
 
 const supakey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0anV1a3podmtzZm9sbmp3dnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Mzk5NTcsImV4cCI6MjA2NzAxNTk1N30.6CUBA7UDHtJ5md2ci1NKJ-40wGBp-oV803es5_pUCs4"
 
-
 const { createClient } = supabase;
 
 let merg = createClient(supaUrl, supakey);
 console.log(merg);
-
-
-
 
 let logbtn = document.querySelector(".logbtn")
 let logpassInp = document.querySelector(".logpass")
@@ -45,20 +41,20 @@ logbtn && logbtn.addEventListener("click", () => {
 
                 loader.style.display = "none"
                 console.log(data);
-                // window.location.href = "home.html"
 
-                if (error) {
-
+                  if(error) throw error
+                if (data) {
+                   window.location.href = "post.html"
                     console.log(error.message);
-
-                } else {
-                    console.log(data);
-                    alert("login successflly")
-
                 }
-                window.location.href = "home.html"
+                 
             } catch (error) {
                 console.log(error.message);
+                if (error.message.includes('invalid login credentials')) {
+					alert('incorrect password');
+				}else if(error.message.includes('invalid login credentials')){
+                    	alert('Please enter a valid email address');
+                }
 
             }
 
@@ -67,3 +63,55 @@ logbtn && logbtn.addEventListener("click", () => {
     }
 })
 
+
+async function displaypro() {
+    try{
+        const {data:{user},error,} = await merg.auth.getUser();
+        console.log(user);
+        
+      if(error) throw error
+        if(user){
+           if( document.querySelector("avater")){
+            document.querySelector("avater").src = user.user_metadata?.avater_Url || "https://www.gravatar.com/avatar/?d=mp"
+            document.querySelector("pro-name").textContent = user.user_metadata?.full_name || user.email
+              document.querySelector("pro-email").textContent = user.user_metadata?.user.email
+           }
+           if(window.location.pathname.includes("index.html")){
+            window.location.href("post.html")
+           }
+           else if(!window.location.pathname.includes("index.html") || window.location.pathname.includes("login.html")){
+            window.location.pathname.includes("index.html")
+           }
+        }
+
+    }catch(error){
+       console.log("sign in error" , error );
+
+       if(!window.location.pathname.includes("index.html") && window.location.pathname.includes("login.html")){
+
+        window.location.href("index.html")
+       }
+       
+    }
+}
+displaypro()
+
+
+let logoutbtn  = document.querySelector("logout");
+
+logoutbtn && logoutbtn.addEventListener("click",async()=>{
+    try{
+    const {error} = await merg.auth.signOut();
+    if(error) throw error
+        window.location.href = "index.html"
+    
+    }catch(error){
+    console.error('Logout error:', error);
+	alert('Logout failed');
+
+
+    }
+})
+
+
+	
